70310dc26bfc30725159ba7d05068de0222bcd9d
Chanjin Lee <chanjin23@naver.com>
2024-03-01 21:21:17 +0900

    엔티티 클래스 개발2

--- src/main/java/jpabookPr/jpashopPr/domain/item/Item.java
+++ src/main/java/jpabookPr/jpashopPr/domain/item/Item.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @Setter
@@ -22,4 +25,7 @@ public abstract class Item {
     private int price;
 
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items") //이방식은 실무에서 유용하지 않다.
+    private List<Category> categories = new ArrayList<>();
 }
