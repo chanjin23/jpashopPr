75d8d9778eb7e8e00a5e038e09ee07274b2dae1e
Chanjin Lee <chanjin23@naver.com>
2024-03-01 18:39:10 +0900

    first commit

--- src/main/java/jpabookPr/jpashopPr/domain/DeliveryStatus.java
+++
@@ -1,5 +0,0 @@
-package jpabookPr.jpashopPr.domain;
-
-public enum DeliveryStatus {
-    READY, COMP
-}
--- src/main/java/jpabookPr/jpashopPr/domain/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package jpabookPr.jpashopPr.domain;
-
-public enum OrderStatus {
-    ORDER,CANCEL
-}
