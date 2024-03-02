eefbd66d5c96f3f2785b90b3c077831bdd5e11f1
Chanjin Lee <chanjin23@naver.com>
2024-03-02 22:20:30 +0900

    주문 리포지토리 개발

---
+++ src/main/java/jpabookPr/jpashopPr/repository/OrderRepository.java
@@ -0,0 +1,28 @@
+package jpabookPr.jpashopPr.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabookPr.jpashopPr.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//
+//    }
+
+}
