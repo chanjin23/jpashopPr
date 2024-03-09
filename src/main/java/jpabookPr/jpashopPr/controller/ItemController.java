001b30eddc54768ef2034c4b37dbe24fefc51d10
Chanjin Lee <chanjin23@naver.com>
2024-03-09 16:20:32 +0900

    회원등록, 회원 목록 조회, 상품 등록

---
+++ src/main/java/jpabookPr/jpashopPr/controller/ItemController.java
@@ -0,0 +1,35 @@
+package jpabookPr.jpashopPr.controller;
+
+import jpabookPr.jpashopPr.domain.item.Book;
+import jpabookPr.jpashopPr.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        Book book = new Book(); //원래는 스태틱 생성자를 사용해서 한번에 설정할 수 있게 하는게 베스트
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+}
--- src/main/java/jpabookPr/jpashopPr/controller/MemberController.java
+++ src/main/java/jpabookPr/jpashopPr/controller/MemberController.java
@@ -1,11 +1,17 @@
 package jpabookPr.jpashopPr.controller;
 
+import jakarta.validation.Valid;
+import jpabookPr.jpashopPr.domain.Address;
 import jpabookPr.jpashopPr.domain.Member;
 import jpabookPr.jpashopPr.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,4 +25,31 @@ public class MemberController {
         model.addAttribute("memberForm", new MemberForm());
         return "members/createMemberForm";
     }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+        /*Valid 어노테이션을 쓰면 validation을 시켜준다.*/
+        /*MemberForm에 오류가 생기면 튕겨버리는데 BindingResult를 하면 result에 담기게된다.*/
+
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
 }
